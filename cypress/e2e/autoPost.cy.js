5ce86e111bbca67a9ececc6aecd9b5e55a1339a7
vinhdx <vinhdao992000@gmail.com>
2023-10-02 17:12:18 +0700

    add demo login facebook

--- cypress/e2e/autoPost.cy.js
+++ cypress/e2e/autoPost.cy.js
@@ -1,4 +1,6 @@
 describe('Tự động đăng bài lên trang cá nhân', () => {
     it('Đăng bài', () => {
-    })
+        cy.visit('https://mbasic.facebook.com/login')
+        cy.login('account.json')
+     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,12 @@
-Cypress.Commands.add("visitPagesInLoop", (urls) => {
-  urls.forEach((url) => {
-    cy.visit(url);
-    // Add your test assertions or actions for each page here
+Cypress.Commands.add("login", (fileData) => {
+  cy.fixture(fileData).then((data) => {
+    cy.get("#objects_container").find("a").eq(1).click();
+    cy.get("input[name='email']").type(data.username);
+    cy.get("input[name='pass']").type(data.password);
+    cy.get("input[name='login']").click();
+    cy.contains("Tin nhắn").click();
+    cy.contains("Đth Vân").click();
+    cy.get("#composerInput").type(data.content);
+    cy.get("input[name='send']").click();
   });
 });
